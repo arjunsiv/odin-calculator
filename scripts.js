ec53cdecd94d0a0890c4372485054f1adf2edccf
Arjun <imarjunsiv@gmail.com>
2023-12-14 09:32:06 +0530

    Calculator html and css complete.

--- scripts.js
+++ scripts.js
@@ -1,1 +1,26 @@
-console.log("Hello");
+const add = (num1, num2)=>{
+    return num1 + num2;
+}
+
+const subtract = (num1, num2)=>{
+    return num1 - num2;
+}
+
+const multiply = (num1, num2)=>{
+    return num1 * num2;
+}
+
+const divide = (num1, num2)=>{
+    if(num2 == 0){
+        return "Divide with zero error";
+    }
+    return num1 / num2;
+}
+
+let num1;
+let num2;
+let operator;
+
+const operate = (num1, num2, operator)=>{
+    add(num1,num2);
+}
